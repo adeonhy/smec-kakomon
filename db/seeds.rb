eefa6a6f63056cc932880bb45ee2db6e7e40e007
adeonhy <adeonhy@gmail.com>
2013-01-14 22:45:11 +0900

    mockup finished 8

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,49 @@ CSV.foreach('db/b1ji2012.csv',headers: true) do |row|
                   selectioncount:#{row[3]},
                   answer:#{row[4]},
                   svg:#{row[5]},
-:                 pdf:#{row[6]},
-                  png:#{row[7]}"
-                 ) if false
+                 pdf:#{row[6]},
+                  png:#{row[7]}") if false
+end
+
+setofsubyears = Setofsubyear.create([
+           {subject:'経済学・経済政策', year:'H24', available:false},
+           {subject:'経済学・経済政策', year:'H23', available:false},
+           {subject:'経済学・経済政策', year:'H22', available:false},
+           {subject:'経済学・経済政策', year:'H21', available:false},
+           {subject:'経済学・経済政策', year:'H20', available:false},
+           {subject:'財務・会計', year:'H24', available:true},
+           {subject:'財務・会計', year:'H23', available:false},
+           {subject:'財務・会計', year:'H22', available:false},
+           {subject:'財務・会計', year:'H21', available:false},
+           {subject:'財務・会計', year:'H20', available:false},
+           {subject:'企業経営理論', year:'H24', available:false},
+           {subject:'企業経営理論', year:'H23', available:false},
+           {subject:'企業経営理論', year:'H22', available:false},
+           {subject:'企業経営理論', year:'H21', available:false},
+           {subject:'企業経営理論', year:'H20', available:false},
+           {subject:'運営管理', year:'H24', available:false},
+           {subject:'運営管理', year:'H23', available:false},
+           {subject:'運営管理', year:'H22', available:false},
+           {subject:'運営管理', year:'H21', available:false},
+           {subject:'運営管理', year:'H20', available:false},
+           {subject:'経営法務', year:'H24', available:false},
+           {subject:'経営法務', year:'H23', available:false},
+           {subject:'経営法務', year:'H22', available:false},
+           {subject:'経営法務', year:'H21', available:false},
+           {subject:'経営法務', year:'H20', available:false},
+           {subject:'経営情報システム', year:'H24', available:false},
+           {subject:'経営情報システム', year:'H23', available:false},
+           {subject:'経営情報システム', year:'H22', available:false},
+           {subject:'経営情報システム', year:'H21', available:false},
+           {subject:'経営情報システム', year:'H20', available:false},
+           {subject:'中小企業経営・政策', year:'H24', available:false},
+           {subject:'中小企業経営・政策', year:'H23', available:false},
+           {subject:'中小企業経営・政策', year:'H22', available:false},
+           {subject:'中小企業経営・政策', year:'H21', available:false},
+           {subject:'中小企業経営・政策', year:'H20', available:false},
+])
+
+CSV.foreach('db/theme_zaimu.csv',headers: true) do |row|
+  Theme.create(themecode: row[0], maintheme:row[1], subtheme:row[2])
+  #puts("themecode: #{row[0]}, maintheme:#{row[1]}, subtheme:#{row[2]}")
 end
