9c9697b97673473e109bef8b0501e923ec2180ce
adeonhy <adeonhy@gmail.com>
2013-01-25 00:53:45 +0900

    20130124 add c1ji2012

--- script/getcsv.rb
+++ script/getcsv.rb
@@ -7,6 +7,7 @@ csv_files = [
   ['db/b1ji2011.csv', '0AnANDRug_3F2dGdVeXNwOHBkY1NxTmdvRUdlV0U3MXc', 1],
   ['db/b1ji2010.csv', '0AnANDRug_3F2dGdVeXNwOHBkY1NxTmdvRUdlV0U3MXc', 2],
   ['db/a1ji2012.csv', '0AnANDRug_3F2dDI3YVpPWmlIbmI2WmJPanN2X2tRR0E', 0],
+  ['db/c1ji2012.csv', '0AnANDRug_3F2dFBrT0wxN29TU3g2VmdZcWo0c1VaMnc', 0],
   ['db/theme_keizai.csv', '0AnANDRug_3F2dFljTkt0anViRFQzOHJWNWhCb3BkaVE', 0],
   ['db/theme_zaimu.csv', '0AnANDRug_3F2dFljTkt0anViRFQzOHJWNWhCb3BkaVE', 1],
   ['db/theme_keiei.csv', '0AnANDRug_3F2dFljTkt0anViRFQzOHJWNWhCb3BkaVE', 3],
